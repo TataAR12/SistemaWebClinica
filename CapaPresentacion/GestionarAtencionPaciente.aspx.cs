6b4db01f67cfa37fc433d9351000c8fe6dd0637c
TataAR12 <angierodri200@gmail.com>
2025-07-10 22:14:08 -0500

    ELIMINAR Y CANCELAR GESTIONAR ATENCION MEDICA

--- CapaPresentacion/GestionarAtencionPaciente.aspx.cs
+++ CapaPresentacion/GestionarAtencionPaciente.aspx.cs
@@ -32,17 +32,39 @@ namespace CapaPresentacion
 
         protected void dlAtencionMedica_ItemCommand(object source, DataListCommandEventArgs e)
         {
+            String IdCita = (e.Item.FindControl("hdIdCita") as HiddenField).Value;
             if (e.CommandName == COMMAND_REGISTER)
 
             {
                 //Realizar el registro de la atención
                 //Redirecion a la pagina de gestionarAtencion 
-                String IdCita = (e.Item.FindControl("hdIdCita") as HiddenField).Value;
-                Response.Redirect("GestionarAtencionCita.aspx?idcita="+IdCita);
+
+                bool response = CitaLN.getInstance().ActualizarCita(Convert.ToInt32(IdCita), "A");
+
+                if (response)
+                {
+                    Response.Redirect("GestionarAtencionCita.aspx?idcita=" + IdCita);
+                }
+                else
+                {
+                    Response.Write("<sript>alert('NO SE PUEDE REALIZAR LA ATENCIÓN DE LA CITA.')</sript>");
+                }
+
             }
             else if(e.CommandName == COMMAND_CANCEL)
             {
                 //Realizar la cancelación de la reserva de cita
+                bool response = CitaLN.getInstance().ActualizarCita(Convert.ToInt32(IdCita), "E");
+
+                if (response)
+                {
+                    //Recargar la informacion
+                    llenarDataList();
+                }
+                else
+                {
+                    Response.Write("<sript>alert('NO SE PUEDE ELIMINAR LA CITA.')</sript>");
+                }
             }
             
         }
